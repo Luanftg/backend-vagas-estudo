906d69b1e09ed85b4748ba5164584359d5860789
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-02-17 08:27:12 -0300

    [refact]: add ServeHandler class to call route

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,10 +1,15 @@
-import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
+import 'server_handler.dart';
+
 void main() async {
+  var _server = ServeHandler();
   final server = await shelf_io.serve(
-      (request) => Response(200, body: 'Bem vindo ao SERVIDOR'),
-      'localhost',
-      8080);
-  print('Servidor iniciado em: http://localhost:8080');
+    _server.handler,
+    'localhost',
+    8080,
+  );
+  print(
+    'Servidor iniciado em: http://localhost:8080',
+  );
 }

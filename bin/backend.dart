68a0757e09beeccd581f576452850a904710d7f3
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-02-17 19:11:25 -0300

    [feat]: create custom server class to initialiize the server

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,15 +1,6 @@
-import 'package:shelf/shelf_io.dart' as shelf_io;
-
-import 'server_handler.dart';
+import 'api/login_api.dart';
+import 'infra/custom_server.dart';
 
 void main() async {
-  var _server = ServeHandler();
-  final server = await shelf_io.serve(
-    _server.handler,
-    'localhost',
-    8080,
-  );
-  print(
-    'Servidor iniciado em: http://localhost:8080',
-  );
+  await CustomServer().initialize(LoginApi().handler);
 }

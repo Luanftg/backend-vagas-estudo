39c54059c3698249f498c139eae6517a1788f8c0
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-02-18 11:07:09 -0300

    [refact]: replace hardcode by CustomEnv class

--- bin/backend.dart
+++ bin/backend.dart
@@ -3,11 +3,17 @@ import 'package:shelf/shelf.dart';
 import 'api/login_api.dart';
 import 'api/vacancy_api.dart';
 import 'infra/custom_server.dart';
+import 'utils/custom_env.dart';
 
 void main() async {
+  CustomEnv.fromFile('.env-dev');
   var cascadeHandler =
       Cascade().add(LoginApi().handler).add(VacancyApi().handler).handler;
   var handler =
       Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
-  await CustomServer().initialize(handler);
+  await CustomServer().initialize(
+    handler: handler,
+    address: await CustomEnv.get<String>(key: 'server_address'),
+    port: await CustomEnv.get<int>(key: 'server_port'),
+  );
 }

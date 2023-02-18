cf23e2ddc9886c2389389e5a9113a5bb34dbb64d
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-02-18 20:55:40 -0300

    [feat]: add vacncyService to Cascade handler

--- bin/backend.dart
+++ bin/backend.dart
@@ -3,12 +3,15 @@ import 'package:shelf/shelf.dart';
 import 'api/login_api.dart';
 import 'api/vacancy_api.dart';
 import 'infra/custom_server.dart';
+import 'services/vacancy_service.dart';
 import 'utils/custom_env.dart';
 
 void main() async {
   CustomEnv.fromFile('.env-dev');
-  var cascadeHandler =
-      Cascade().add(LoginApi().handler).add(VacancyApi().handler).handler;
+  var cascadeHandler = Cascade()
+      .add(LoginApi().handler)
+      .add(VacancyApi(VacancyService()).handler)
+      .handler;
   var handler =
       Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
   await CustomServer().initialize(

f5e1ab7a4202fda14440af8e386f80f8feced7a0
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-02-18 20:57:28 -0300

    [refact]: add serialization to model

--- bin/models/vacancy_model.dart
+++ bin/models/vacancy_model.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
 class VacancyModel {
-  final int id;
+  final int? id;
   final String title;
   final String description;
   final String image;
@@ -19,4 +21,37 @@ class VacancyModel {
   String toString() {
     return 'VacancyModel(id: $id, title: $title, description: $description, image: $image, createdAt: $createdAt, updatedAt: $updatedAt)';
   }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'id': id});
+    result.addAll({'title': title});
+    result.addAll({'description': description});
+    result.addAll({'image': image});
+    result.addAll({'createdAt': createdAt.millisecondsSinceEpoch});
+    if (updatedAt != null) {
+      result.addAll({'updatedAt': updatedAt!.millisecondsSinceEpoch});
+    }
+
+    return result;
+  }
+
+  factory VacancyModel.fromMap(Map<String, dynamic> map) {
+    return VacancyModel(
+      map['id']?.toInt() ?? 0,
+      map['title'] ?? '',
+      map['description'] ?? '',
+      map['image'] ?? '',
+      DateTime.fromMillisecondsSinceEpoch(map['createdAt']),
+      map['updatedAt'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(map['updatedAt'])
+          : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory VacancyModel.fromJson(String source) =>
+      VacancyModel.fromMap(json.decode(source));
 }

858ab165a758cb76197c4bbe9d351054a5b1fbf6
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-02-18 07:22:43 -0300

    [feat]: create cascade Handler  to add another router and add logqrequest by middleware

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,6 +1,13 @@
+import 'package:shelf/shelf.dart';
+
 import 'api/login_api.dart';
+import 'api/vacancy_api.dart';
 import 'infra/custom_server.dart';
 
 void main() async {
-  await CustomServer().initialize(LoginApi().handler);
+  var cascadeHandler =
+      Cascade().add(LoginApi().handler).add(VacancyApi().handler).handler;
+  var handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
+  await CustomServer().initialize(handler);
 }

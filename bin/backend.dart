53d3fcbb04eaf4bebc1eadfef53cc56a837aee73
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-02-23 18:13:52 -0300

    [feat]: initialize Dependency Injector

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,19 +1,26 @@
 import 'package:shelf/shelf.dart';
-
 import 'api/login_api.dart';
 import 'api/vacancy_api.dart';
 import 'infra/custom_server.dart';
-import 'services/vacancy_service.dart';
+import 'infra/dependency_injector/injects.dart';
+import 'infra/middleware_interception.dart';
 import 'utils/custom_env.dart';
 
 void main() async {
   CustomEnv.fromFile('.env-dev');
+
+  final _di = Injects.initialize();
+
   var cascadeHandler = Cascade()
-      .add(LoginApi().handler)
-      .add(VacancyApi(VacancyService()).handler)
+      .add(_di.get<LoginApi>().getHandler())
+      .add(_di.get<VacancyApi>().getHandler(isSecurity: true))
       .handler;
-  var handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
+
+  var handler = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(MiddlewareInterception().middleware)
+      .addHandler(cascadeHandler);
+
   await CustomServer().initialize(
     handler: handler,
     address: await CustomEnv.get<String>(key: 'server_address'),

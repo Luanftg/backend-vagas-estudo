a21d1c2b65d932bf2583d58b75f31245d8acd4a8
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-02-23 18:18:21 -0300

    [feat]: create middlewares interception and create set mime type app/json

---
+++ bin/infra/middleware_interception.dart
@@ -0,0 +1,9 @@
+import 'package:shelf/shelf.dart';
+
+class MiddlewareInterception {
+  Middleware get middleware => createMiddleware(
+        responseHandler: (Response res) => res.change(
+          headers: {'content-type': 'application/json'},
+        ),
+      );
+}
